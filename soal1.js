77d717fd89505ae451656752921289f884440d43
Ciputra Pangestu <ciputrapangestu2810@gmail.com>
2020-06-12 11:09:23 +0200

    first commit

---
+++ soal1.js
@@ -0,0 +1,7 @@
+let jobIT = ['software developer','cyber security','devops','data analyst','network engineer']
+console.log(jobIT.length)
+console.log(jobIT)
+
+jobIT.map( jobIT1 =>{
+    return document.write(`<li>${jobIT1}</li>`)
+})
